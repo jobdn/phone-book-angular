211dd7c945f63d4a7a73552a649c4aba92c65bbb
Danila Pisarev <jobdan@bk.ru>
2022-07-06 20:43:01 +0300

    feat: add tailwind

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -5,7 +5,6 @@ import { Contact } from '../models/contact.model';
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.scss'],
 })
 export class ContactListComponent {
   contacts: Contact[] = [
